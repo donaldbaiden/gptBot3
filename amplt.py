22cb5610aae168cf3f04eddceae8247facdda0a7
Misha <fakefriks.gg@gmail.com>
2024-06-26 11:21:52 +0300

    removed print

--- amplt.py
+++ amplt.py
@@ -10,7 +10,7 @@ executor = ThreadPoolExecutor()
 client = Amplitude(api_key=settings.amplitude_api_key)
 
 
-#
+
 # def callback_fun(e, code, message):
 #     """A callback function"""
 #     print(e)
@@ -26,14 +26,12 @@ def send_amplitude_event(user_id: str, event_type: str, event_properties: dict):
             event = BaseEvent(event_type=event_type, user_id=user_id, event_properties=event_properties)
             client.track(event)
             lst = client.flush()
-            print(lst)
         except Exception as e:
             print(f"Error sending event to Amplitude: {e}")
 
     executor.submit(send_event_in_thread)
 
 
-# Пример использования
 async def log_user_action(user_id: str, action: str):
     event_type = 'user_action'
     event_properties = {'action': action}
